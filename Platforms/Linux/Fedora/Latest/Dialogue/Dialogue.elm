3bdcabea368fd0f945bea89c0df9958655d74c81
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-11 19:09:30 -0800

    Added support for Fedora Linux (60 files)
    Added support for Fedora Linux, containing 60 files in various file formats.

---
+++ Platforms/Linux/Fedora/Latest/Dialogue/Dialogue.elm
@@ -0,0 +1,31 @@
+{- Start of script -}
+{- The games dialouge -}
+-- I am not too experienced with the Elm language, so this script may not function properly or at all. Please report bugs in GitHub issues. It is mostly pseudocode right now
+
+-- These are required
+{- No libraries to link yet -}
+
+#Tutorial {
+  VeryBeginning : Action
+  VeryBeginning == /Intro.elm
+  Dialogue1 : String
+  Dialogue1 == "@^Poppy{ Welcome to the forest."
+  Dialogue2 : String
+  Dialogue2 == "@^Poppy{ Let's start by planting a seed."
+  Dialogue3 : String
+  Dialogue3 == "@^Poppy{ Water the seed a few more times."
+  Dialogue4 : String
+  Dialogue4 == "@^Poppy{ And a new leaf has been turned."
+  Dialogue5 : String
+  Dialogue5 == "@^Poppy{ Look, some animals have shown up. Why not bring them onto your tree?"
+  -- I can't write more dialogue right now
+}
+
+{-
+File info
+File type: Elm source code (*.elm)
+File version: 1 (2021, Friday, December 10th at 6:30 pm)
+Line count (including blank lines and compiler line): 32
+-}
+
+{- End of script -}
