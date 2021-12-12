3305f1e6966d3f33608bc61e188cab7eb6867b65
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-11 19:15:55 -0800

    Added support for Fedora 30 (60 files)
    Added the support directory for Fedora 30, containing 60 files in various file formats.

---
+++ Platforms/Linux/Fedora/30/Currency/OldVersions/1/1-100/CURRENCY_V1.elm
@@ -0,0 +1,29 @@
+{- Start of script -}
+{- Currency for the game -}
+-- I am not too experienced with the Elm language, so this script may not function properly or at all. Please report bugs in GitHub issues.
+
+seeds : Integer -- Normal currency
+elmTokens : Integer -- Special currency (but not costing money)
+seedEnoughN type = String
+seedEnoughN =
+  "You do not have enough tree seeds to perform this action"
+SeedEnoughK type = String
+seedEnoughK =
+  "$Confirm"
+elmTokenEnoughN type = String
+elmTokenEnoughN =
+  "You do not have enough elm tokens to perform this action"
+elmTokenEnoughK type = String
+elmTokenEnoughK =
+  "Are you sure you want to spend $X on this item?"
+  if "$ACTION" == "$YES"
+    "$Confirm"
+  else =
+    "$Cancel"
+
+{- File info
+File type: Elm source code (*elm)
+File version: 1 (2021, Monday, December 6th at 6:25 pm)
+Line count (including blank lines and compiler line): 30
+
+{- End of script -}
